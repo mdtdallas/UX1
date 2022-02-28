6592b3da454de85e60ba90833391d279ba4da4ad
Dallas Little <37045574+mdtdallas@users.noreply.github.com>
2022-02-28 11:49:43 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 fetch("nav.html")
-.then()
+.then(response => response.text())
+.then(html => {
+    document.getElementById("navbar").innerHTML = html
+});
      
+fetch("catForm.html")
+.then(response => response.text())
+.then(html => {
+    document.getElementById("catForm").innerHTML = html
+});
+
+fetch('showForm.html')
+.then(res => res.text())
+.then (html => {
+    document.getElementById('showForm').innerHTML = html
+});
+
+fetch('awardform.html')
+.then(res => res.text())
+.then (html => {
+    document.getElementById('awardForm').innerHTML = html
+});
+
+fetch('userForm.html')
+.then(res => res.text())
+.then (html => {
+    document.getElementById('userForm').innerHTML = html
+});
+
+fetch('loginForm.html')
+.then(res => res.text())
+.then (html => {
+    document.getElementById('loginForm').innerHTML = html
+});
